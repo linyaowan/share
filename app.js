c69ac0cad3e76abf673e3b75062d26eb9cf13606
linyaowan <7324000532@qq.com>
2019-08-05 09:44:47 +0800

    修改
    样式修改；
    获取授权 个人信息保存接口；

--- app.js
+++ app.js
@@ -11,47 +11,17 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-    // // 登录
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //     console.log(res);
-    //   }
-    // })
-    // 登录
-    wx.login({
-      
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log(res);
-        wx.request({
-          url: "https://xcx.keeko.ai/api/getOpenId",
-          data: {
-            code: res.code
-          },
-          method: 'GET',
-          success: function (result) {
-            var data = result.data;
-            console.log(data);
-            if (data.code == 200) {
-              console.log("openId:" + data.msg)
-               self.globalData.openId = data.msg
-               console.log(self.globalData.openId)
-            }
-          }
-        })
-      }
-    })
+
+
     //获取用户信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          //说明用户信息已经授权，可直接调用wx.getUserInfo获取，
           wx.getUserInfo({
             success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
+              console.log(res)
+              this.globalData.userInfo = res.userInfo;
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -62,6 +32,7 @@ App({
         }
       }
     })
+    
   }
 
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -16,13 +16,77 @@ Page({
   data: {},
   login() {
     const that = this
+    // 登录
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //     console.log(res);
+    //   }
+    // })
+    // 登录
     wx.login({
-      success() {
+      success: res => {
         app.globalData.hasLogin = true
         that.setData({
           hasLogin: true
         })
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: "https://xcx.keeko.ai/api/getOpenId",
+          data: {
+            code: res.code
+          },
+          method: 'GET',
+          success: function (result) {
+            var data = result.data;
+            if (data.code == 200) {
+              self.globalData.openId = data.msg      
+            }
+          }
+        })
+      }
+    })
+  },
+
+  selfGetInfo(e) {
+    // this.login();
+    //出现授权弹窗，用户点了允许
+    if (e.detail.userInfo) {
+      app.globalData.userinfo = e.detail.userInfo;
+      this.setData({
+        avatarUrl: e.detail.userInfo.avatarUrl
+      })
+      wx.request({
+        url: "https://xcx.keeko.ai/api/saveUserInfo",
+        data: {
+          avatarUrl: e.detail.userInfo.avatarUrl,
+          userNickName: e.detail.userInfo.userNickName,
+          userGender: e.detail.userInfo.userGender,
+          userCity: e.detail.userInfo.userCity,
+          userProvince: e.detail.userInfo.userProvince
+        },
+        method: 'post',
+        success: function (result) {
+        }
+      })
+    } else {
+      //用户点了拒绝 ，则wx.getUserInfo这个方法，不会出现弹窗，直接进入fail的回调中
+      this.selfModal();
+    }
+  },
+  selfModal: function () {   
+    wx.showModal({
+      title: '提示',
+      content: '请接受授权，否则会影响你的使用效果',
+      showCancel: false,//不显示取消按钮
+      success: res => {
+        if (res.confirm) {
+          console.log('用户点击了确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
       }
     })
   }
+    
 })
